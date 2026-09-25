e6674f7d37b01c3a62a64d034035d18d5b8dd54c
Nilesh <nileshprasad758@gmail.com>
2026-09-25 11:06:16 +0530

    Add files via upload

---
+++ pinMode.ino
@@ -0,0 +1,25 @@
+/*
+  pinMode 
+
+  Syatax
+  pinMode(pin , mode);
+
+  pin-Arduino pin 
+  mode- Input/ Output 
+
+Example - Blink and LED 
+
+pinMode(13,OUTPUT)
+
+
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
